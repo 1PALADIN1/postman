1a83732eda29a4a8f06f9a0115ce64b2741cc3ae
1PALADIN1 <sonikx94@yandex.ru>
2019-06-23 12:18:00 +0300

    Исправлена проблема с перетеранием лучшего прогресса худшим

--- Assets/Scripts/Helper/ProgressLoader.cs
+++ Assets/Scripts/Helper/ProgressLoader.cs
@@ -74,11 +74,13 @@ namespace Core.Helper
         /// <param name="stars">Количество собранных звёзд</param>
         public void SaveFinishLevel(int level, int stars)
         {
-            //TODO поправить ошибку с перезатираением лучшего прогресса
-
             if (_levelChecker.HasLevel(level))
             {
-                if (_progress.ContainsKey(level)) _progress[level] = new ProgressInfo(stars, true);
+                if (_progress.ContainsKey(level))
+                {
+                    if (stars > _progress[level].CollectedStars)
+                        _progress[level] = new ProgressInfo(stars, true);
+                }
                 else
                     _progress.Add(level, new ProgressInfo(stars, true));
                 SaveGameProgress();
